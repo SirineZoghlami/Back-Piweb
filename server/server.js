e45330b06d17bf1f52828aaa331188cc003411b5
Marwen-Neji <78695522+Marwen-Neji@users.noreply.github.com>
2024-04-25 15:28:59 +0100

    port 8000

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-const { PORT = 5000 ,dbpassword} = process.env;
+const { PORT = 8000 ,dbpassword} = process.env;
 
 app.use(helmet());
 app.use(bodyParser.json()); 
